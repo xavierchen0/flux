9b20f5abfea18121a4b1f3b11f840864a3752615
Xavier Chen <xaviernehc@gmail.com>
2026-01-24 17:15:01 +0800

    Initial Commit

---
+++ flux/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flux
+//
+//  Created by Xavier Chen on 24/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flux/fluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  fluxApp.swift
+//  flux
+//
+//  Created by Xavier Chen on 24/1/26.
+//
+
+import SwiftUI
+
+@main
+struct fluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
